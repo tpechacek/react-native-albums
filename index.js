34db04e36841fd2e030081df3f98d7a71f0122e3
Taylor Pechacek <taylortpechacek@gmail.com>
2019-01-09 21:50:36 +1100

    finishing for the night - small edits

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ import { Text, AppRegistry } from 'react-native';
 import Header from './src/components/header';
 
 // Create a component
-const App = () => <Header />;
+const App = () => <Header headerText={'Albums'} />;
 
 // Render it to the device
 AppRegistry.registerComponent('albums', () => App);
