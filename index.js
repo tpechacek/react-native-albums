ce03661e13ea885939cb4f80869d8df732ff9838
Taylor Pechacek <taylortpechacek@gmail.com>
2019-01-13 17:14:31 +1100

    scaffolding for new class based component AlbumsList

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
-// /** @format */
-
-// import { AppRegistry } from 'react-native';
-// import App from './App';
-// import { name as appName } from './app.json';
-
-// AppRegistry.registerComponent(appName, () => App);
-
 // Import a library to help create a component
 import React from 'react';
-import { Text, AppRegistry } from 'react-native';
+import { AppRegistry, View } from 'react-native';
 import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
 
 // Create a component
-const App = () => <Header headerText={'Albums'} />;
+const App = () => (
+  <View>
+    <Header headerText={'Albums'} />
+    <AlbumList />
+  </View>
+);
 
 // Render it to the device
 AppRegistry.registerComponent('albums', () => App);
