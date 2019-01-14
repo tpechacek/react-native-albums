4d87de6cf0053f52aca103d942475ab5f02d1db8
Taylor Pechacek <taylortpechacek@gmail.com>
2019-01-14 22:18:38 +1100

    couple of small changes. Adding component level state and fetching a request using axios to show how this is being wired up

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
 class AlbumList extends Component {
+  // Some initial state which is just an empty object.
+  // Empty list of albums when we first load our component.
+  state = { albums: [] };
+
+  componentWillMount() {
+    // console.log('componentWillMount in AlbumList');
+
+    axios
+      .get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(response => this.setState({ albums: response.data }));
+  }
+
   render() {
+    console.log(this.state);
+
     return (
       <View>
         <Text>Album List!!!!</Text>
