55e530e4b85fdb2099ec8619d0a8786e001696a1
Taylor Pechacek <taylortpechacek@gmail.com>
2019-01-27 09:26:16 +1100

    last pieces, learning how to use a common Button component and Linking. Finished with the app now

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Text, TouchableOpacity } from 'react-native';
 
-const Button = () => {
+const Button = ({ onPress, children }) => {
   const { buttonStyle, textStyle } = styles;
 
   return (
-    <TouchableOpacity style={buttonStyle}>
-      <Text style={textStyle}>Click me</Text>
+    <TouchableOpacity onPress={onPress} style={buttonStyle}>
+      <Text style={textStyle}>{children}</Text>
     </TouchableOpacity>
   );
 };
